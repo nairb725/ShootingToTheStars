ae83c9c51c6801a86459800dad9171f777c1642c
Brian <nairb725@gmail.com>
2023-11-30 19:19:58 +0100

    sabotage de lubin

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -69,7 +69,6 @@ public class GameManager : MonoBehaviour
     {
         if (Instance == null) // If there is no instance already
         {
-            DontDestroyOnLoad(gameObject); // Keep the GameObject, this component is attached to, across different scenes
             Instance = this;
         }
         else if (Instance != this) // If there is already an instance and it's not `this` instance
